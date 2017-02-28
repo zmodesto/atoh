a4ad7d523575dbc4cbf58584a79d3cc599939cf2
Zach Modesto <zmodesto@gmail.com>
2017-02-28 16:13:31 -0500

    fix a thing. atoh completed, i think.

--- app/app.module.ts
+++ app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
@@ -27,7 +28,8 @@ import { HeroService } from './hero.service';
       AppComponent,
       DashboardComponent,
       HeroDetailComponent,
-      HeroesComponent
+      HeroesComponent,
+      HeroSearchComponent
   ],
   providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
